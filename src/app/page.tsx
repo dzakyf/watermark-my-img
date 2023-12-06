19082cceec8bf05658ac07c7226b57484b645436
dzakyf <dzakyf@gmail.com>
2023-12-06 17:03:19 +0700

    finished implement download feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 'use client'
 
-import { useRef } from "react"
+import { useState,  useRef } from "react"
+import html2canvas from "html2canvas"
 
 
 export default function Main() {
   const canvasRef = useRef(null),
-        canvasContainerRef = useRef(null)
-  
+        canvasContainerRef = useRef(null),
+        downloadRef = useRef(null),
+        [text, setText] = useState(""),
+        [isImageLoaded, setImageLoaded] = useState(false)
+      
   function drawCanvas(e){
-    const canvas = canvasRef.current,
-          container = canvasContainerRef.current,
-          context = canvas.getContext('2d'),
-          file = e.target.files[0],
+    setText("")
+    setImageLoaded(true)
+    const file = e.target.files[0],
           fileSrc = URL.createObjectURL(file),
-
-    img = new Image()
+          canvas = canvasRef.current,
+          context = canvas.getContext('2d'),
+          container = canvasContainerRef.current,
+          img = new Image()
+    
     img.src = fileSrc 
     img.onload = function (){
       const hImg = img.height,
@@ -27,13 +33,29 @@ export default function Main() {
       //will fit to container 
       let ratio = Math.min((hContainer/hImg),(wContainer/wImg))
 
-      //resize canvas based on ratio   
+      // resize canvas based on ratio   
       canvas.height = hContainer
       canvas.width = wImg*ratio 
       
-      //paint to document 
+      // paint to canvas 
       context.drawImage(img,0,0, wImg, hImg, 0,0, wImg*ratio, hImg*ratio)
+
     }
+  }
+
+  function handleTextChange(e){
+    setText(e.target.value)
+  }
+
+  function handleDownload(){
+    let fileName = "res"
+
+    html2canvas(downloadRef.current).then(function(canvas){
+      const downloadLink = document.createElement('a')
+      downloadLink.download = fileName
+      downloadLink.href = canvas.toDataURL("image/png;base64") 
+      downloadLink.click()
+    })
   }
 
 
@@ -43,33 +65,40 @@ export default function Main() {
       <div className="col-span-1">
        <div className="w-full h-[36rem] shadow-lg pb-full rounded-xl bg-white">
         <div className="flex flex-col p-12 gap-2">
+        {isImageLoaded && 
+        <button className="border" onClick={handleDownload}>Download</button>
+        }
         <input id="open-file" type="file" accept="image/png, image/jpeg" onChange={drawCanvas}></input>
         <p>Input</p>
-        <textarea className="border"></textarea>
-        <p>Position</p>
+        <textarea id="input-text" onChange={handleTextChange} className="border"></textarea>
+        {/* <p>Position</p>
         <select name="position" id="position-select" className="border">
           <option value="">--Please choose an option--</option>
           <option value="top">Atas</option>
           <option value="middle">Tengah</option>
           <option value="bottom">Bawah</option>
-        </select>
+        </select> */}
+        <p>Enlarge</p>
+        <input id="opacity" name="opacity-range" type="range" min="0" max="100" className="border"></input>
         <p>Opacity/Transparency</p>
         <input id="opacity" name="opacity-range" type="range" min="0" max="100" className="border"></input>
-        <p>Zoom</p>
-        <input id="opacity" name="opacity-range" type="range" min="0" max="100" className="border"></input>
-        <p>Rotate</p>
+        
+        {/* <p>Rotate</p> */}
         <div className="flex flex-row gap-2">
-        <button className="border rounded-full p-1">90 CCW</button>
+        {/* <button className="border rounded-full p-1">90 CCW</button>
         <button className="border rounded-full p-1">90 CW</button>
         <button className="border rounded-full p-1">45 CCW</button>
-        <button className="border rounded-full p-1">45 CW</button>
+        <button className="border rounded-full p-1">45 CW</button> */}
         </div>
        </div>
        </div>
       </div>
       <div className="col-span-3">
-       <div ref={canvasContainerRef} className="flex justify-center w-full h-[36rem] shadow-lg pb-full rounded-xl bg-white">
-        <canvas id="canvas"  ref={canvasRef}></canvas>
+      <div ref={canvasContainerRef} className="flex justify-center w-full h-[36rem] shadow-lg pb-full rounded-xl bg-white static">
+        <div id="download-ref" ref={downloadRef}> 
+          <p id="draggable-text" className="absolute">{text}</p>
+          <canvas id="canvas"  ref={canvasRef}></canvas>
+        </div>
        </div>
       </div>
       </div>
